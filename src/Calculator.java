02e11849cd18b67e7adeaa8778ef2ed2478ede71
kevin rodriguez <kevinrodriguez@kevins-MacBook-Air.local>
2022-02-07 21:07:26 -0500

    added support for floating number inputs (also forgot to add an escape character for one of my strings)

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,26 +1,53 @@
 import java.util.Scanner;
 
 public class Calculator {
-
+		
 	public static void main(String[] args) {
+		int operandUno, operandDos;
+		double operandOne, operandTwo;
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Whole or double numbers?");
+		String in = sc.nextLine();
+		if (in.equalsIgnoreCase("whole")) {
 		System.out.println("Enter in a number:");
-		int operandUno = sc.nextInt();
+		  operandUno = sc.nextInt();
 		System.out.println("Enter in a number:");
-		int operandDos = sc.nextInt();
-		System.out.println("Would you like to: \n1. add \2. subtract \n3. multiply \n4. divide");
-		int input = sc.nextInt();
-		if (input == 1) {
-		System.out.print((operandUno + operandDos));
-	} else if (input == 2) {
-		System.out.print((operandUno - operandDos));
-	} else if(input == 3) {
-		System.out.print((operandUno * operandDos));
-	} else if (input == 4) {
-		System.out.print((operandUno / operandDos));
-	} else {
-		System.out.println("invalid input");
+		 operandDos = sc.nextInt();
+		 System.out.println("Would you like to: \n1. add \n2. subtract \n3. multiply \n4. divide");
+			int input = sc.nextInt();
+			if (input == 1) {
+			System.out.print((operandUno + operandDos));
+		} else if (input == 2) {
+			System.out.print((operandUno - operandDos));
+		} else if(input == 3) {
+			System.out.print((operandUno * operandDos));
+		} else if (input == 4) {
+			System.out.print((operandUno / operandDos));
+		} else {
+			System.out.println("invalid input");
+			}
+		} else if (in.equalsIgnoreCase("double")) {
+			System.out.println("Enter in a number:");
+			 operandOne = sc.nextDouble();
+			System.out.println("Enter in a number:");
+			 operandTwo = sc.nextDouble();
+			 System.out.println("Would you like to: \n1. add \n2. subtract \n3. multiply \n4. divide");
+				int input = sc.nextInt();
+				if (input == 1) {
+				System.out.print((operandOne + operandTwo));
+			} else if (input == 2) {
+				System.out.print((operandOne - operandTwo));
+			} else if(input == 3) {
+				System.out.print((operandOne * operandTwo));
+			} else if (input == 4) {
+				System.out.print((operandOne / operandTwo));
+			} else {
+				System.out.println("invalid input");
+				}
+		} else {
+			System.out.println("invalid input");
 		}
-	}
-	
+	}	
 }
+	
+
