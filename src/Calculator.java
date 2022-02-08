187805eba9d1db5cb67554e458eb517a89f2e627
kevin rodriguez <kevinrodriguez@kevins-MacBook-Air.local>
2022-02-07 20:55:43 -0500

    added multiplacation and division operations

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,12 +8,16 @@ public class Calculator {
 		int operandUno = sc.nextInt();
 		System.out.println("Enter in a number:");
 		int operandDos = sc.nextInt();
-		System.out.println("Would you like to: \n1. add \2. subtract");
+		System.out.println("Would you like to: \n1. add \2. subtract \n3. multiply \n4. divide");
 		int input = sc.nextInt();
 		if (input == 1) {
 		System.out.print((operandUno + operandDos));
 	} else if (input == 2) {
 		System.out.print((operandUno - operandDos));
+	} else if(input == 3) {
+		System.out.print((operandUno * operandDos));
+	} else if (input == 4) {
+		System.out.print((operandUno / operandDos));
 	} else {
 		System.out.println("invalid input");
 		}
